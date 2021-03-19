131135d41ee8bcf7c79ecd4cc691ba12bdf51547
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2021-03-18 21:01:01 -0700

    Update parse_modes_VASP.py
    Mode type (real/imaginary) is now returned in addition to the eigenvectors

--- phonon_tools_VASP/parse_modes_VASP.py
+++ phonon_tools_VASP/parse_modes_VASP.py
@@ -3,6 +3,7 @@ from numpy import zeros,array
 def parse_modes_VASP(outcar):
     with open(outcar, 'r') as outcar:
         modes=[]
+        mode_types=[]
         while True:
             line=outcar.readline()
             if not line:
@@ -10,10 +11,14 @@ def parse_modes_VASP(outcar):
             if 'NIONS' in line:
                 atomnum=int(line.split()[11])
             elif 'THz' in line:
+                if '/i' in line:
+                    mode_types.append('imaginary')
+                else:
+                    mode_types.append('real')
                 modes.append(zeros((atomnum,3)))
                 line=outcar.readline()
                 for i in range(atomnum):
                     line=outcar.readline().split()
                     modes[-1][i]+=array([float(line[j]) for j in range(3,6)])
     
-    return modes
+    return modes, mode_types
